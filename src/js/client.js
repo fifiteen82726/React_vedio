ab7dbe66886e36d8e29d83d5ea31a322b9fc1809
fifiteen82726 <fifiteen82726@gmail.com>
2016-08-31 00:44:22 +0800

    finish v3, 學會如何把component 拆開

--- src/js/client.js
+++ src/js/client.js
@@ -1,20 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./component/layout";
 
-class Layout extends React.Component{
-	constructor() {
-		super();
-		this.name = "coda"
-	}
-	getVal(){
-		return "will" 
-	}
-	render() {
-		return (
-			<div>It's {this.name}</div>
-		);
-	}
-}
 
 const app = document.getElementById('app')
 ReactDOM.render(<Layout/>, app);
---
+++ src/js/component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "./Header/title"
+
+export default class Header extends React.Component{
+  render() {
+  	return (
+  		<div>
+  			Header!
+			<Title />
+  		</div>
+  	);
+  }
+}
